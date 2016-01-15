1981d478e8138a2b700dd27f562c1489eeaf057d
Victor Kurauchi <victorkurauchi@gmail.com>
2016-01-15 12:11:06 -0200

    Components

--- admin/src/components/InvalidFieldType.js
+++ admin/src/components/InvalidFieldType.js
@@ -1,11 +1,11 @@
 var React = require('react');
 
 module.exports = React.createClass({
-	
-	displayName: 'InvalidFieldType',
-	
-	render: function() {
-		return <div className="alert alert-danger">Invalid field type <strong>{this.props.type}</strong> at path <strong>{this.props.path}</strong></div>;
-	}
-	
+
+  displayName: 'InvalidFieldType',
+
+  render: function() {
+    return <div className="alert alert-danger">Tipo de campo inválido <strong>{this.props.type}</strong> at path <strong>{this.props.path}</strong></div>;
+  }
+
 });
